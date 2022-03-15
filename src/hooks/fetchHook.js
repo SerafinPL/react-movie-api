dcd658c1b90c60058170dac57700708ff761e483
serafinpl <ser.burberibar@gmail.com>
2022-03-15 13:57:36 +0100

    add files

---
+++ src/hooks/fetchHook.js
@@ -0,0 +1,45 @@
+import { useState, useCallback } from "react";
+
+const useFetchData = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = useCallback(( search) => {
+    fetch(
+      `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_CODE}&fields=${search}`,
+      {
+        method: "GET",
+      
+    }
+    )
+      .then((res) => {
+        
+        if (res.ok) {
+           
+          return res.json();
+        } else {
+          throw Error(res.statusText);
+        }
+      })
+      .then((res) => {
+        
+        setData(res);
+        
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+
+        if (err = 'Too Many Requests'){
+
+          window.alert('Zbyt dużo zapytań do Bazy')
+        }
+      });
+  }, []);
+
+  return {
+    fetchData: fetchData,
+    data: data,
+  };
+};
+
+export default useFetchData;
